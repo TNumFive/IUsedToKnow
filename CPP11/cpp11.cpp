869957c097f863b1b5d98dcb68d761116f1acb51
TNumFive <determinetof@gmail.com>
2021-04-18 21:39:36 +0800

    cpp11

---
+++ CPP11/cpp11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <map>
+
+long fibonacci(unsigned n)
+{
+    if (n < 2) return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main()
+{
+    auto start = std::chrono::steady_clock::now();
+    std::cout << "f(42) = " << fibonacci(42) << '\n';
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+}
