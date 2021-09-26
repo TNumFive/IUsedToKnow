f6013f1cf55313de93ed812f553df2e1b66af1d8
TNumFive <determinetof@gmail.com>
2021-09-27 00:56:16 +0800

    有向无向图拆分

--- 考研/数据结构/Graph1.cpp
+++ 考研/数据结构/Graph1.cpp
@@ -1,50 +1,53 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
+#include <list>
 using namespace std;
 
 #define MAX_VERTEX_NUM 10
 #define EDGE_TYPE_MAX INT32_MAX
 typedef int VertexType;
 typedef int EdgeType;
+//采用无压缩邻接矩阵，经修改，此处图操作即算法针对无向图
 typedef struct
 {
+    //简单使用对应需要里的节点值不为0代表该节点存在，简化判断
     VertexType vertex[MAX_VERTEX_NUM];
     //0表示无限，>0表示权
     EdgeType edge[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
     size_t vexNum, arcNum;
 } Graph;
-//下列方法针对有向图，无向图加边需手动处理
 bool Adjacent(Graph &g, VertexType x, VertexType y)
 {
     if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
     {
         return false;
     }
+    //由于无向，只做一边的判断
     if (g.edge[x][y] > 0)
     {
         return true;
     }
     return false;
 }
-VertexType Neighbors(Graph &g, VertexType x)
+list<VertexType> Neighbors(Graph &g, VertexType x)
 {
+    list<VertexType> l;
     if (x < 0 || x >= MAX_VERTEX_NUM)
     {
-        return -1;
+        return l;
     }
     for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
     {
-        if (g.edge[x][i] > 0 || g.edge[i][x] > 0)
+        if (g.edge[x][i] > 0)
         {
-            return i;
+            l.push_back(i);
         }
     }
-    return -1;
+    return l;
 }
 bool InsertVertex(Graph &g, VertexType x)
 {
-    // printf("insert %d\n", x);
     if (x < 0 || x >= MAX_VERTEX_NUM)
     {
         return false;
@@ -73,10 +76,6 @@ bool DeleteVertex(Graph &g, VertexType x)
         if (g.edge[i][x] > 0)
         {
             g.edge[i][x] = 0;
-            g.arcNum--;
-        }
-        if (g.edge[x][i] > 0)
-        {
             g.edge[x][i] = 0;
             g.arcNum--;
         }
@@ -85,7 +84,6 @@ bool DeleteVertex(Graph &g, VertexType x)
 }
 bool AddEdge(Graph &g, VertexType x, VertexType y)
 {
-    // printf("add edge <%d,%d>\n", x, y);
     if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
     {
         return false;
@@ -97,6 +95,7 @@ bool AddEdge(Graph &g, VertexType x, VertexType y)
     if (g.edge[x][y] == 0)
     {
         g.edge[x][y] = 1;
+        g.edge[y][x] = 1;
         g.arcNum++;
     }
     return true;
@@ -110,6 +109,7 @@ bool RemoveEdge(Graph &g, VertexType x, VertexType y)
     if (g.edge[x][y] > 0)
     {
         g.edge[x][y] = 0;
+        g.edge[y][x] = 0;
         g.arcNum--;
     }
     return true;
@@ -163,6 +163,7 @@ bool SetEdgeValue(Graph &g, VertexType x, VertexType y, EdgeType e)
         return false;
     }
     g.edge[x][y] = e;
+    g.edge[y][x] = e;
     return true;
 }
 
@@ -171,34 +172,27 @@ void Visit(VertexType v)
     cout << v << " ";
 }
 
-void RandomInitGraph(Graph &g, bool isDirected = false)
+void RandomInitGraph(Graph &g)
 {
     memset(&g, 0, sizeof(Graph));
     int temp1, temp2;
     for (size_t i = 0; i < MAX_VERTEX_NUM * 2; i++)
     {
         temp1 = rand() % MAX_VERTEX_NUM;
-        // cout<<temp1<<endl;
         InsertVertex(g, temp1);
     }
     for (size_t i = 0; i < MAX_VERTEX_NUM * 2; i++)
     {
         temp1 = rand() % MAX_VERTEX_NUM;
         temp2 = rand() % MAX_VERTEX_NUM;
-        // cout<<temp1<<" "<<temp2<<endl;
         AddEdge(g, temp1, temp2);
-        if (!isDirected)
-        {
-            AddEdge(g, temp2, temp1);
-            g.arcNum--;
-        }
     }
 }
 
 void DisplayGraph(Graph &g)
 {
     cout << "vertex: ";
-    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
     {
         if (g.vertex[i] != 0)
         {
@@ -206,18 +200,25 @@ void DisplayGraph(Graph &g)
         }
     }
     cout << endl;
-    cout << "   ";
-    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    cout << "\033[4m   \033[0m";
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
     {
-        printf("\033[4m%2ld \033[0m", i);
+        printf("\033[4m%2d \033[0m", i);
     }
     cout << endl;
-    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
     {
-        printf("%-2ld|", i);
-        for (size_t j = 0; j < MAX_VERTEX_NUM; j++)
+        printf("%-2d│", i);
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
         {
-            printf("%2d ", g.edge[i][j]);
+            if (g.edge[i][j] != 0)
+            {
+                printf("%2d ", g.edge[i][j]);
+            }
+            else
+            {
+                printf(" - ");
+            }
         }
         cout << endl;
     }
@@ -286,7 +287,7 @@ void DFSTraverse(Graph &g)
     }
 }
 
-void RandomSetWeight(Graph &g, bool isDirected = false)
+void RandomSetWeight(Graph &g)
 {
     for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
     {
@@ -295,8 +296,7 @@ void RandomSetWeight(Graph &g, bool isDirected = false)
             if (g.edge[i][j] > 0)
             {
                 g.edge[i][j] = rand() % (MAX_VERTEX_NUM - 1) + 1;
-                if (!isDirected)
-                    g.edge[j][i] = g.edge[i][j];
+                g.edge[j][i] = g.edge[i][j];
             }
         }
     }
@@ -342,10 +342,7 @@ void Prim(Graph &g, Graph &t)
         }
         InsertVertex(t, nextVertex2);
         AddEdge(t, nextVertex1, nextVertex2);
-        AddEdge(t, nextVertex2, nextVertex1);
-        g.arcNum--; //由于是无向图，需要手动减去一条边数
         SetEdgeValue(t, nextVertex1, nextVertex2, minDist);
-        SetEdgeValue(t, nextVertex2, nextVertex1, minDist);
     }
 }
 
@@ -400,154 +397,9 @@ void Kruskal(Graph &g, Graph &t)
         }
         //查询完所有边后，开始加入边
         AddEdge(t, v1, v2);
-        AddEdge(t, v2, v1);
-        t.arcNum--; //由于是无向图，需要手动减去一条边数
         SetEdgeValue(t, v1, v2, minDist);
-        SetEdgeValue(t, v2, v1, minDist);
         //处理并查集
         ufs[UFSFindRoot(ufs, v2)] = UFSFindRoot(ufs, v1);
-    }
-}
-
-void Dijkstra(Graph &g, VertexType first = -1)
-{
-    EdgeType dist[MAX_VERTEX_NUM] = {0};
-    //用于记录顶点和边的集合
-    Graph d;
-    memset(&d, 0, sizeof(d));
-    VertexType v, next;
-    if (first == -1)
-    {
-        //选取第一个顶点
-        for (v = 0; v < MAX_VERTEX_NUM; v++)
-        {
-            if (g.vertex[v] != 0)
-            {
-                break;
-            }
-        }
-        first = v;
-    }
-    else
-    {
-        if (g.vertex[first] == 0)
-        {
-            return;
-        }
-        v = first;
-    }
-    InsertVertex(d, v);
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        if (i != v)
-        {
-            dist[i] = GetEdgeValue(g, v, i);
-        }
-    }
-    EdgeType minDist;
-    while (d.vexNum < g.vexNum)
-    {
-        minDist = EDGE_TYPE_MAX;
-        for (v = 0; v < MAX_VERTEX_NUM; v++)
-        {
-            //距离最短，且未被加入
-            if (dist[v] != 0 && minDist > dist[v] && d.vertex[v] == 0)
-            {
-                minDist = dist[v];
-                next = v;
-            }
-        }
-        if (minDist == EDGE_TYPE_MAX)
-        {
-            break;
-        }
-        InsertVertex(d, next);
-        //更新到各点的距离
-        for (EdgeType j = FirstNeighbor(g, next); j >= 0; j = NextNeighbor(g, next, j))
-        {
-            if (dist[j] > dist[next] + GetEdgeValue(g, next, j) || dist[j] == 0)
-            {
-                dist[j] = dist[next] + GetEdgeValue(g, next, j);
-            }
-        }
-        // break;
-    }
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        cout << i << "   ";
-    }
-    cout << endl;
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        if (dist[i] == 0)
-        {
-            cout << "-   ";
-        }
-        else
-        {
-            cout << dist[i] << "   ";
-        }
-    }
-    cout << endl;
-}
-
-void Floyd(Graph &g)
-{
-    EdgeType a[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
-        {
-            a[i][j] = GetEdgeValue(g, i, j);
-        }
-    }
-    for (VertexType k = 0; k < MAX_VERTEX_NUM; k++)
-    {
-        if (g.vertex[k] == 0)
-        {
-            continue;
-        }
-        for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-        {
-            if (g.vertex[i] == 0)
-            {
-                continue;
-            }
-            for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
-            {
-                if (g.vertex[j] == 0)
-                {
-                    continue;
-                }
-                EdgeType distance = GetEdgeValue(g, i, k) + GetEdgeValue(g, k, j);
-                if (GetEdgeValue(g, i, j) > distance)
-                {
-                    SetEdgeValue(g, i, j, distance);
-                }
-            }
-        }
-    }
-    cout << "   ";
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        printf("\033[4m%2d \033[0m", i);
-    }
-    cout << endl;
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        printf("%-2d|", i);
-        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
-        {
-            if (a[i][j] != EDGE_TYPE_MAX)
-            {
-                printf("%2d ", a[i][j]);
-            }
-            else
-            {
-                cout << " - ";
-            }
-        }
-        cout << endl;
     }
 }
 
@@ -570,33 +422,5 @@ int main(int argc, char const *argv[])
     cout << "Kruskal" << endl;
     Kruskal(g, k);
     DisplayGraph(k);
-    Graph d;
-    RandomInitGraph(d, true);
-    RandomSetWeight(d, true);
-    cout << "Graph d" << endl;
-    DisplayGraph(d);
-    cout << "Dijkstra" << endl;
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        if (d.vertex[i] != 0)
-        {
-            cout << "at vertex: " << i << endl;
-            Dijkstra(d, i);
-        }
-    }
-    cout << "Floyd" << endl;
-    //使用EDGE_TYPE_MAX代替0为无穷的代表值，方便后续运算
-    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
-    {
-        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
-        {
-
-            if (GetEdgeValue(d, i, j) == 0)
-            {
-                SetEdgeValue(d, i, j, EDGE_TYPE_MAX);
-            }
-        }
-    }
-    Floyd(d);
     return 0;
 }
---
+++ 考研/数据结构/Graph2.cpp
@@ -0,0 +1,445 @@
+/**
+ * Graph针对的是无向图的一些算法，使用0作为无穷标识符，在有向图比较中比较不和
+ * 此文件定义了最大可表示值来代表无穷
+ */
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <list>
+using namespace std;
+
+#define MAX_VERTEX_NUM 10
+#define EDGE_TYPE_MAX INT32_MAX
+typedef int VertexType;
+typedef int EdgeType;
+typedef struct
+{
+    //简单使用对应需要里的节点值不为0代表该节点存在，简化判断
+    VertexType vertex[MAX_VERTEX_NUM];
+    //使用EDGE_TYPE_MAX代表无穷=》后续可考虑浮点数，天然自带无穷；
+    EdgeType edge[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+    size_t vexNum, arcNum;
+} Graph;
+
+bool Adjacent(Graph &g, VertexType x, VertexType y)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.edge[x][y] != EDGE_TYPE_MAX)
+    {
+        return true;
+    }
+    return false;
+}
+
+list<VertexType> Neighbors(Graph &g, VertexType x)
+{
+    list<VertexType> l;
+    if (x < 0 || x >= MAX_VERTEX_NUM)
+    {
+        return l;
+    }
+    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        if (g.edge[x][i] > 0)
+        {
+            l.push_back(i);
+        }
+    }
+    return l;
+}
+
+bool InsertVertex(Graph &g, VertexType x)
+{
+    if (x < 0 || x >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.vertex[x] == 0)
+    {
+        g.vexNum++;
+        g.vertex[x] = 1;
+    }
+    return true;
+}
+
+bool DeleteVertex(Graph &g, VertexType x)
+{
+    if (x < 0 || x >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.vertex[x] > 0)
+    {
+        g.vexNum--;
+        g.vertex[x] = 0;
+    }
+    //去边
+    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        if (g.edge[i][x] > 0)
+        {
+            g.edge[i][x] = 0;
+            g.edge[x][i] = 0;
+            g.arcNum--;
+        }
+        if (g.edge[x][i] > 0)
+        {
+            g.edge[x][i] = 0;
+            g.arcNum--;
+        }
+    }
+    return true;
+}
+
+bool AddEdge(Graph &g, VertexType x, VertexType y)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.vertex[x] == 0 || g.vertex[y] == 0)
+    {
+        return false;
+    }
+    if (g.edge[x][y] == EDGE_TYPE_MAX)
+    {
+        g.edge[x][y] = 1;
+        g.arcNum++;
+    }
+    return true;
+}
+
+bool RemoveEdge(Graph &g, VertexType x, VertexType y)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.edge[x][y] != EDGE_TYPE_MAX)
+    {
+        g.edge[x][y] = EDGE_TYPE_MAX;
+        g.arcNum--;
+    }
+    return true;
+}
+
+VertexType FirstNeighbor(Graph &g, VertexType x)
+{
+    if (x < 0 || x >= MAX_VERTEX_NUM)
+    {
+        return -1;
+    }
+    for (size_t i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        if (g.edge[x][i] != EDGE_TYPE_MAX)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+VertexType NextNeighbor(Graph &g, VertexType x, VertexType y)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return -1;
+    }
+    for (size_t i = y + 1; i < MAX_VERTEX_NUM; i++)
+    {
+        if (g.edge[x][i] != EDGE_TYPE_MAX)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+EdgeType GetEdgeValue(Graph &g, VertexType x, VertexType y)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return -1;
+    }
+    return g.edge[x][y];
+}
+
+bool SetEdgeValue(Graph &g, VertexType x, VertexType y, EdgeType e)
+{
+    if (x < 0 || y < 0 || x >= MAX_VERTEX_NUM || y >= MAX_VERTEX_NUM)
+    {
+        return false;
+    }
+    if (g.vertex[x] == 0 || g.vertex[y] == 0)
+    {
+        return false;
+    }
+    g.edge[x][y] = e;
+    return true;
+}
+
+void RandomInitGraph(Graph &g)
+{
+    memset(&g, 0, sizeof(Graph));
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+        {
+            g.edge[i][j] = EDGE_TYPE_MAX;
+        }
+    }
+    int temp1, temp2, temp3;
+    for (size_t i = 0; i < MAX_VERTEX_NUM * 2; i++)
+    {
+        temp1 = rand() % MAX_VERTEX_NUM;
+        InsertVertex(g, temp1);
+    }
+    for (size_t i = 0; i < MAX_VERTEX_NUM * 2; i++)
+    {
+        temp1 = rand() % MAX_VERTEX_NUM;
+        temp2 = rand() % MAX_VERTEX_NUM;
+        temp3 = rand() % (MAX_VERTEX_NUM - 1) + 1;
+        AddEdge(g, temp1, temp2);
+        SetEdgeValue(g, temp1, temp2, temp3);
+    }
+}
+
+void DisplayGraph(Graph &g)
+{
+    cout << "vertex: ";
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        if (g.vertex[i] != 0)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    cout << "\033[4m   \033[0m";
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        printf("\033[4m%2d \033[0m", i);
+    }
+    cout << endl;
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        printf("%-2d│", i);
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+        {
+            if (g.edge[i][j] != EDGE_TYPE_MAX)
+            {
+                printf("%2d ", g.edge[i][j]);
+            }
+            else
+            {
+                printf(" - ");
+            }
+        }
+        cout << endl;
+    }
+}
+
+void Dijkstra(Graph &g, EdgeType dist[], VertexType path[], VertexType first = -1)
+{
+    //初始化距离数组，路径数组
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        dist[i] = EDGE_TYPE_MAX;
+        path[i] = -1;
+    }
+    VertexType v, next;
+    if (first == -1)
+    {
+        //选取第一个顶点
+        for (v = 0; v < MAX_VERTEX_NUM; v++)
+        {
+            if (g.vertex[v] != 0)
+            {
+                break;
+            }
+        }
+        first = v;
+    }
+    else
+    {
+        if (g.vertex[first] == 0)
+        {
+            return;
+        }
+        v = first;
+    }
+    //用于记录顶点和边的集合
+    Graph d;
+    memset(&d, 0, sizeof(d));
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+        {
+            d.edge[i][j] = EDGE_TYPE_MAX;
+        }
+    }
+    //加入第一个顶点
+    InsertVertex(d, v);
+    //初始化数值
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        //不更新自己，不更新距离为无穷的
+        if (i != v && GetEdgeValue(g, v, i) != EDGE_TYPE_MAX)
+        {
+            dist[i] = GetEdgeValue(g, v, i);
+            path[i] = i;
+        }
+    }
+    EdgeType minDist;
+    //所有节点完成则退出
+    while (d.vexNum < g.vexNum)
+    {
+        minDist = EDGE_TYPE_MAX;
+        for (v = 0; v < MAX_VERTEX_NUM; v++)
+        {
+            //距离最短，且未被加入
+            if (minDist > dist[v] && d.vertex[v] == 0)
+            {
+                minDist = dist[v];
+                next = v;
+            }
+        }
+        //找不到下一条边了
+        if (minDist == EDGE_TYPE_MAX)
+        {
+            break;
+        }
+        //找到下一个节点
+        InsertVertex(d, next);
+        //更新到各点的距离
+        for (EdgeType j = FirstNeighbor(g, next); j >= 0; j = NextNeighbor(g, next, j))
+        {
+            if (dist[j] - dist[next] > +GetEdgeValue(g, next, j) && j != first)
+            {
+                dist[j] = dist[next] + GetEdgeValue(g, next, j);
+                path[j] = next;
+            }
+        }
+    }
+}
+
+void Floyd(Graph &g)
+{
+    //初始化数组
+    EdgeType a[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+        {
+            a[i][j] = GetEdgeValue(g, i, j);
+        }
+    }
+    //循环查找
+    for (VertexType k = 0; k < MAX_VERTEX_NUM; k++)
+    {
+        if (g.vertex[k] == 0)
+        {
+            continue;
+        }
+        for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+        {
+            if (g.vertex[i] == 0)
+            {
+                continue;
+            }
+            for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+            {
+                if (g.vertex[j] == 0 || i == j)
+                {
+                    continue;
+                }
+                //注意溢出问题，在a、b、c均为正数时 "a>b+c => a-b>c"
+                if (a[i][j] - a[i][k] > a[k][j])
+                {
+                    a[i][j] = a[i][k] + a[k][j];
+                }
+            }
+        }
+    }
+    cout << "\033[4m   \033[0m";
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        printf("\033[4m%2d \033[0m", i);
+    }
+    cout << endl;
+    for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+    {
+        printf("%-2d│", i);
+        for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+        {
+            if (a[i][j] != EDGE_TYPE_MAX)
+            {
+                printf("%2d ", a[i][j]);
+            }
+            else
+            {
+                cout << " - ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Graph g;
+    cout << "Random init directed graph" << endl;
+    RandomInitGraph(g);
+    DisplayGraph(g);
+    {
+        cout << "Dijstra - min distance - path matrix" << endl;
+        EdgeType dist[MAX_VERTEX_NUM];
+        VertexType path[MAX_VERTEX_NUM];
+        cout << "\033[4m   \033[0m";
+        for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+        {
+            printf("\033[4m%2d \033[0m", i);
+        }
+        cout << "\t\033[4m   \033[0m";
+        for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+        {
+            printf("\033[4m%2d \033[0m", i);
+        }
+        cout << endl;
+        for (VertexType i = 0; i < MAX_VERTEX_NUM; i++)
+        {
+            Dijkstra(g, dist, path, i);
+            printf("%-2d│", i);
+            for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+            {
+                if (dist[j] != EDGE_TYPE_MAX)
+                {
+                    printf("%2d ", dist[j]);
+                }
+                else
+                {
+                    printf(" - ");
+                }
+            }
+            printf("\t%-2d│", i);
+            for (VertexType j = 0; j < MAX_VERTEX_NUM; j++)
+            {
+                if (path[j] != -1)
+                {
+                    printf("%2d ", path[j]);
+                }
+                else
+                {
+                    printf(" - ");
+                }
+            }
+            cout << endl;
+        }
+    }
+    cout << "Floyd - min distance" << endl;
+    Floyd(g);
+    return 0;
+}
