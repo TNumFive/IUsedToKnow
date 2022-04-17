f3dbc1e301e28a35741b5e29ccd43bff204742df
TNumFive <fangxw@nuaa.edu.cn>
2022-04-17 21:52:44 +0800

    test extra blank be removed in pub key allow passwd for easy use

--- Ubuntu/set_up.sh
+++ Ubuntu/set_up.sh
@@ -15,13 +15,9 @@ cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak &&
 echo 'curl sshd config'&&
 curl -o /etc/ssh/sshd_config https://raw.githubusercontent.com/TNumFive/IUsedToKnow/master/Ubuntu/sshd_config &&
 # create pubkey dir
-dir=`pwd` && cd ~ && mkdir .ssh || cd .ssh &&
+mkdir ~/.ssh >
 # set pubkey
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCeP/TTknmYhljBVcOheq07+HCDdAXDi8/Umfi/r2bnnTFG0j0u7hwa4PGaEZypn+TUv4czOwqmU9L/ppfEjZz165gFlHAJWlIcpBtrOqigcSCde4ihFBod6uIA+wSCOicg1wLFb0g52a1mVEIVo3dz151bjZdbjE3Yn2DoFSIjnSLK58hh3xdUHrnP5cld2h/xAXx48PLrsLZ6l8cwpL/gQJX2/W5eyKQr5Qxrem8DbPtVv+OZj0v0jgtFZdALKJLXIdNbLbZ/m6DI1nKMvlzvSoWDNlQTuV3rI4Ftl3sjq/2VgAgOUrwXh4N1j2mC2jq38taz33mYviAbjuHbIGX4UuM3VG7eBTbyTULt10Ng8hX8m9QZd1JEHfeAFNCfyFQY0mzjr0ANEZEM4UVZGXHWmKLYXzxHWutV2KH6zqgK19uV+7IUTXtCVrYnynPjWljVXT0S2jcojW7whWOD713HcMluay06gll2HAsJJX2BCn6/FhIHVIaXTT6sxOapXC8= fangxw@NITRO5
-
-' > authorized_keys &&
-# return back
-cd ${dir} &&
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCeP/TTknmYhljBVcOheq07+HCDdAXDi8/Umfi/r2bnnTFG0j0u7hwa4PGaEZypn+TUv4czOwqmU9L/ppfEjZz165gFlHAJWlIcpBtrOqigcSCde4ihFBod6uIA+wSCOicg1wLFb0g52a1mVEIVo3dz151bjZdbjE3Yn2DoFSIjnSLK58hh3xdUHrnP5cld2h/xAXx48PLrsLZ6l8cwpL/gQJX2/W5eyKQr5Qxrem8DbPtVv+OZj0v0jgtFZdALKJLXIdNbLbZ/m6DI1nKMvlzvSoWDNlQTuV3rI4Ftl3sjq/2VgAgOUrwXh4N1j2mC2jq38taz33mYviAbjuHbIGX4UuM3VG7eBTbyTULt10Ng8hX8m9QZd1JEHfeAFNCfyFQY0mzjr0ANEZEM4UVZGXHWmKLYXzxHWutV2KH6zqgK19uV+7IUTXtCVrYnynPjWljVXT0S2jcojW7whWOD713HcMluay06gll2HAsJJX2BCn6/FhIHVIaXTT6sxOapXC8= fangxw@NITRO5' > ~/.ssh/authorized_keys &&
 # restart sshd
 systemctl restart sshd &&
 # backup default vimrc
